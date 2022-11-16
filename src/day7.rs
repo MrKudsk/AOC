7521b007c5a060ffddc3319c880eaf79fc946bc4
Kim <kim.kudsk@gmail.com>
2022-11-16 13:52:21 +0100

    Done Day 7

--- src/day7.rs
+++ src/day7.rs
@@ -6,13 +6,46 @@ pub fn run() {
     println!("day 7, output 2: {}", parser2(INPUT));
 }
 
+fn get_number(s: &str) -> Vec<i32> {
+    s.lines()
+        .next()
+        .unwrap()
+        .split(',')
+        .map(|i| i.parse().unwrap())
+        .collect()
+}
+
 pub fn parser1(s: &str) -> i32 {
-    dbg!(s);
-    todo!();
+    let mut puzzle = get_number(s);
+
+    let nth = puzzle.len() / 2;
+    let (_, midian, _) = &puzzle.select_nth_unstable(nth);
+    let midian: i32 = **midian;
+    let cost = puzzle.iter().fold(0, |acc, pos| i32::abs(pos - midian) + acc);
+    //dbg!(cost);
+
+    cost    
 }
 
 pub fn parser2(s: &str) -> i32 {
-    todo!();
+    let puzzle = get_number(s);
+
+    let min = puzzle.iter().min().unwrap();
+    let max = puzzle.iter().max().unwrap();
+    let cost = (*min..=*max)
+        .into_iter()
+        .map(|mid| {
+            puzzle.iter()
+                .map(|pos| {
+                    let dist = (pos - mid).abs();
+                    (1 + dist) * dist / 2
+                })
+                .sum()
+        })
+        .min()
+        .unwrap();
+
+    cost    
 }
 
 #[cfg(test)]
@@ -23,12 +56,12 @@ mod test {
 
     #[test]
     fn first() {
-        assert_eq!(parser1(INPUT), 4512)
+        assert_eq!(parser1(INPUT), 37)
     }
 
     #[test]
     fn second() {
-        assert_eq!(parser2(INPUT), 1924)
+        assert_eq!(parser2(INPUT), 168)
     }
 }
 
