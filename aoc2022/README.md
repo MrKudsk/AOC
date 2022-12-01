874ca7da387cfbea6ba48fea248b8ae6139a6057
Kim <kim.kudsk@gmail.com>
2022-12-01 13:53:30 +0100

    Day 1 2022 # 54354 and 52824

--- aoc2022/README.md
+++ aoc2022/README.md
@@ -10,7 +10,10 @@
     cargo watch -q -c -w src/ -x 'test --bin dayXX'
 ```
 
-### --- Day 01 ---
+### --- Day 01: Calorie Counting ---
+* Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?
+* Find the top three Elves carrying, the most Calories. How many Calories are those 
+  Elves carrying in total?
 
 ### --- Day 02 ---
 
--- aoc2022/src/bin/day01.rs
+++ aoc2022/src/bin/day01.rs
@@ -1,4 +1,6 @@
 #[allow(unused_variables)]
+use itertools::Itertools;
+use itertools::max;
 
 const INPUT: &'static str = include_str!("../../input/01.txt");
 
@@ -8,26 +10,57 @@ fn main() {
 }
 
 fn parse1(s: &str) -> usize {
-    todo!()
-}
+    let tmp: Vec<i32> = s.split("\n\n")
+            .map(|elf| elf.lines()
+                .map(|l| l.parse::<i32>().unwrap_or(0))
+                .collect_vec()
+                .iter().sum())
+        .collect_vec();
+    //dbg!(&tmp);
+    max(tmp).unwrap() as usize
+}    
 
 fn parse2(s: &str) -> usize {
-    todo!()
+    let mut tmp: Vec<i32> = s.split("\n\n")
+            .map(|elf| elf.lines()
+                .map(|l| l.parse::<i32>().unwrap_or(0))
+                .collect_vec()
+                .iter().sum())
+        .collect_vec();
+    tmp.sort();
+    
+    tmp.iter()
+        .rev()
+        .take(3)
+        .sum::<i32>() as usize
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
 
-    const INPUT: &str = "";
+    const INPUT: &str = "1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000";
 
     #[test]
     fn first() {
-        assert_eq!(parse1(INPUT), 1);
+        assert_eq!(parse1(INPUT), 24000);
     }
 
     #[test]
     fn second() {
-        assert_eq!(parse2(INPUT), 1);
+        assert_eq!(parse2(INPUT), 45000);
     }
 }
