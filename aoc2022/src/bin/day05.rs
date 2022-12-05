b4f05652a8f00a035196e1bc7a2150ebe209604f
Kim <kim.kudsk@gmail.com>
2022-12-05 12:58:39 +0100

    Day 05 part 1 42638

--- aoc2022/src/bin/day05.rs
+++ aoc2022/src/bin/day05.rs
@@ -1,3 +1,5 @@
+//use std::{collections::HashMap};
+
 #[allow(unused_variables)]
 
 const INPUT: &'static str = include_str!("../../input/05.txt");
@@ -7,8 +9,74 @@ fn main() {
     println!("day 05, output 2: {}", parse2(INPUT));
 }
 
+/*
+fn test() {
+    for (i, p) in pos.iter().enumerate() {
+        //dbg!(i, p);
+        let stack_line = puzzel_iter.clone()
+            //.lines()
+            .filter(|l| l.chars().nth(*p).unwrap() != ' ')
+            .map(|l| l.chars().nth(*p).unwrap())
+            .collect::<Vec<char>>();
+        stack.insert(i, stack_line);
+    };
+}
+*/ 
+
+#[derive(Debug, Clone)]
+struct Move {
+    num: u8,
+    from: u8,
+    to: u8,
+}
+
 fn parse1(s: &str) -> String {
-    todo!()
+    let (puzzel, moves_p) = s.split_once("\n\n").unwrap();
+    dbg!(puzzel);
+    //let mut stack = HashMap::new();
+    let mut puzzel_iter = puzzel.lines()
+        .rev();
+    let pos_line = puzzel_iter.next().unwrap();
+    let pos = pos_line.chars()
+        .filter(|c| c != &' ')
+        .map(|c| {
+            pos_line.find(c).unwrap()
+        })
+        .collect::<Vec<usize>>();
+    let mut stack = pos.iter()
+        .map(|p| {
+            puzzel_iter.clone()
+                .filter(|l| l.chars().nth(*p).unwrap() != ' ')
+                .map(|l| l.chars().nth(*p).unwrap())
+                .collect::<Vec<char>>()
+        })
+        .collect::<Vec<Vec<char>>>();
+    let moves = moves_p.lines()
+        .map(|l|{
+            let tmp = l.split(" ")
+            .collect::<Vec<&str>>();
+            //dbg!(&tmp);
+            let num = tmp[1].parse::<u8>().unwrap();
+            let from = tmp[3].parse::<u8>().unwrap() - 1;
+            let to = tmp[5].parse::<u8>().unwrap() - 1;
+            //dbg!(num);
+            Move{num, from, to}
+        })
+        .collect::<Vec<Move>>();
+    dbg!(&moves);
+
+    for m in moves {
+        (1..=m.num).for_each(|_| {
+            let tmp = &stack[m.from as usize].pop().unwrap();
+            stack[m.to as usize].push(*tmp);
+        })
+    }
+    dbg!(&stack);
+    stack.iter()
+        .map(|v| {
+            v.last().unwrap()
+        })
+        .collect::<String>()
 }
 
 fn parse2(_s: &str) -> usize {
