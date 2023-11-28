8343a7b6da53649518ccf2cfeac06f84d5167602
Kim Kudsk <kim.kudsk@gmail.com>
2023-11-28 22:44:45 +0100

    aoc::parsing

---
+++ aoc2022/src/util/parsing.rs
@@ -0,0 +1,14 @@
+use std::fmt::Debug;
+use std::str::FromStr;
+
+pub fn lines_to_vec<R>(input: impl AsRef<str> -> Result<Vec<R>, R::Err>
+where
+    R: FromStr,
+    <R as FromStr>::Err: Debug,
+{
+    return input
+        .as_ref()
+        .lines()
+        .map(|line| line.parse::<R>())
+        .collect::<Result<_, _>>();
+}
