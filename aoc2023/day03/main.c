bef98ea866cfadea11b139a76d146aca4ba7e862
Kim Kudsk <kim.kudsk@gmail.com>
2024-10-02 00:13:49 +0200

    reading the file

--- aoc2023/day03/main.c
+++ aoc2023/day03/main.c
@@ -1,6 +1,47 @@
+#include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char *argv[]) {
-  printf("hello world");
-  return 0;
+#define return_defer(value)                                                    \
+  do {                                                                         \
+    result = (value);                                                          \
+    goto defer;                                                                \
+  } while (0)
+
+#define INPUT "./day03/test.txt"
+
+static char *contents;
+
+int read_from_file(char *filename) {
+  FILE *f = NULL;
+  int result = 0;
+  {
+    f = fopen(filename, "r");
+    if (f == NULL) {
+      fprintf(stderr, "%s: ERROR: could not read the file: %s\n", filename,
+              strerror(errno));
+      return_defer(errno);
+    }
+
+    fseek(f, 0, SEEK_END);
+    int length = ftell(f);
+    fseek(f, 0, SEEK_SET);
+
+    contents = malloc(sizeof(char) * length + 1);
+    fread(contents, 1, length, f);
+    contents[length] = '\0';
+  }
+defer:
+  fclose(f);
+  return result;
+}
+
+int main(void) {
+  int result;
+
+  result = read_from_file(INPUT);
+
+  printf("%s\n", contents);
+  return result;
 }
