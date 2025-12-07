ab72d25835444fad373775e62271c3766afe56f9
Kim Kudsk <kim.kudsk@gmail.com>
2025-12-07 22:15:51 +0100

    2025 day04 init

---
+++ aoc2025/day04/index.ts
@@ -0,0 +1,22 @@
+
+import fs from "fs/promises";
+
+function partOne(banks: string[]) {
+  let total = 0;
+  banks.forEach((bank) => {
+  });
+  console.log("PartOne", total);
+}
+
+function partTwo(banks: string[]) {
+  let total = 0;
+  banks.forEach((bank) => {
+  });
+  console.log("PartTwo", total);
+}
+
+const input = await fs.readFile("./import-sample.txt", "utf-8");
+// const input = await fs.readFile("./import.txt", "utf-8");
+
+partOne(input.slice(0, -1).split("\n"));
+// partTwo(input.slice(0, -1).split("\n"));
