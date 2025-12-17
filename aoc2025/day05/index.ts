7a98ba18cce3bd049f818eec69460b0516521596
Kim Kudsk <kim.kudsk@gmail.com>
2025-12-18 00:21:25 +0100

    2025 day05 partOne

--- aoc2025/day05/index.ts
+++ aoc2025/day05/index.ts
@@ -1,9 +1,22 @@
 
 import fs from "fs/promises";
 
-
 function partOne(grid: string[]) {
   let total = 0;
+
+  const ranges = grid![0].split("\n").map(line => line.split("-").map(Number));
+  // console.log(ranges);
+  const numbers = grid![1].split("\n").map(Number);
+  // console.log(numbers);
+  for (const n of numbers) {
+    if (n == 0) continue;
+    for (const r of ranges) {
+      if (n >= r![0] && n <= r![1]) {
+        total++;
+        break;
+      }
+    }
+  }
   console.log("PartOne", total);
 }
 
@@ -13,8 +26,9 @@ function partTwo(grid: string[]) {
   console.log("PartTwo", total);
 }
 
-const input = await fs.readFile("./import-sample.txt", "utf-8");
-// const input = await fs.readFile("./import.txt", "utf-8");
+// const input = await fs.readFile("./import-sample.txt", "utf-8");
+const input = await fs.readFile("./import.txt", "utf-8");
 
-partOne(input.slice(0, -1).split("\n"));
+
+partOne(input.split("\n\n"));
 // partTwo(input.slice(0, -1).split("\n"));
